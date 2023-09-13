36a374782504aeafde514d744075c53e5c85ba45
Gwangyeong Jeong <wjdrhkddud2@gmail.com>
2023-09-13 23:43:52 +0900

    Template method 패턴 코드 작성

--- BehavioralPattern/src/com/gmail/wjdrhkddud2/Main.java
+++ BehavioralPattern/src/com/gmail/wjdrhkddud2/Main.java
@@ -5,6 +5,8 @@ import com.gmail.wjdrhkddud2.mediator.Activity;
 import com.gmail.wjdrhkddud2.strategy.Strategies;
 import com.gmail.wjdrhkddud2.strategy.Strategy;
 import com.gmail.wjdrhkddud2.strategy.StrategyFactory;
+import com.gmail.wjdrhkddud2.templatemethod.factorymethod.AlgorithmFactory;
+import com.gmail.wjdrhkddud2.templatemethod.factorymethod.AlgorithmType;
 
 public class Main {
 
@@ -14,6 +16,11 @@ public class Main {
 
         Context mediator = new Activity();
         mediator.event();
+
+        AlgorithmFactory algorithmFactory = new AlgorithmFactory();
+        algorithmFactory.create(AlgorithmType.A).execute();
+        algorithmFactory.create(AlgorithmType.B).execute();
+
     }
 
 }
